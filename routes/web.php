e36d624720b0e45aa277b391b9c6063aab060a59
nasgunawann <nasgunawann@gmail.com>
2025-10-01 16:06:54 +0700

    Mengenal View Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Hello Laravel";
+    return view('index');
 });
 
-Route::get('/about', function () {
-    return "Ini adalah halaman <b>About</b>";
+Route::get('about', function () {
+    $nama = 'Nasrullah Gunawan';
+    // return view('profile', compact('nama'));
+    return view('about')->with('nama', $nama);
 });
 
-Route::get('/mahasiswa', function () {
-    return "Ini adalah halaman <b>Mahasiswa</b>";
+Route::get('mahasiswa', function () {
+    return view('mahasiswa');
 });
 
 Route::get('/profile', function () {
-    $nama = "Nasrullah Gunawan";
-    return "Ini adalah halaman <b>Profile</b>. Nama saya adalah " . $nama;
+    return view('profile');
 });
